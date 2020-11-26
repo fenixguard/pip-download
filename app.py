5df80af4da7a390e31fe15e862f2fe3fc2968644
Sergey Volkov <fenixguard1993@gmail.com>
2020-11-26 21:38:51 +0300

    added ruquirements download

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 import io
 import os
 import zipfile
+import shutil
+from subprocess import Popen
+from typing import Optional
 
 import pip
-import tempfile
 from flask import Flask, render_template, send_file
 from flask_bootstrap import Bootstrap
+from werkzeug.datastructures import FileStorage
 
-from parsers import pip_parser
+from parsers import pip_parser, requirements_parser, download_parser
 
 app = Flask(__name__)
 Bootstrap(app)
 
 BASE_DIR = os.getcwd()
+PACKAGE_DIR = 'packages'
+REQUIREMENTS_DIR = 'requirements'
 
 
 @app.route('/')
@@ -21,29 +26,103 @@ def view_index():
     return render_template('index.html')
 
 
-@app.route('/get_dependencies', methods=['POST', 'GET'])
+@app.route('/requirements')
+def view_requirements():
+    return render_template('requirements.html')
+
+
+def create_clear_directories():
+    try:
+        shutil.rmtree(os.path.join(BASE_DIR, PACKAGE_DIR))
+    except Exception as error:
+        print(error)
+    try:
+        shutil.rmtree(os.path.join(BASE_DIR, REQUIREMENTS_DIR))
+    except Exception as error:
+        print(error)
+    try:
+        os.mkdir(os.path.join(BASE_DIR, PACKAGE_DIR))
+    except Exception as error:
+        print(error)
+    try:
+        os.mkdir(os.path.join(BASE_DIR, REQUIREMENTS_DIR))
+    except Exception as error:
+        print(error)
+
+
+def get_zip_stream():
+    files = os.listdir(os.path.join(BASE_DIR, PACKAGE_DIR))
+    memory_zip = io.BytesIO()
+    with zipfile.ZipFile(memory_zip, mode='w', compression=zipfile.ZIP_DEFLATED) as zip_file:
+        for file in files:
+            with open(os.path.join(BASE_DIR, PACKAGE_DIR, file), mode='rb') as ff:
+                zip_file.writestr(file, ff.read())
+    memory_zip.seek(0)
+    return memory_zip
+
+
+@app.route('/requirements/post_requirements', methods=['POST'])
+def post_requirements():
+    args = requirements_parser.parse_args()
+    options: str = args['options']
+    if 'requirements' not in args:
+        return '', 500
+    requirements: Optional[FileStorage] = args['requirements']
+    create_clear_directories()
+    requirements.save(os.path.join(BASE_DIR, REQUIREMENTS_DIR, 'requirements.txt'))
+    if requirements and options:
+        pip.main(['download',
+                  '-d', os.path.join(BASE_DIR, PACKAGE_DIR),
+                  '-r', os.path.join(BASE_DIR, REQUIREMENTS_DIR, 'requirements.txt'),
+                  options])
+        # Popen(f"pip download -d {os.path.join(BASE_DIR, PACKAGE_DIR)} -r {os.path.join(BASE_DIR, REQUIREMENTS_DIR, 'requirements.txt')} {options}").wait()
+    elif requirements and not options:
+        pip.main(['download',
+                  '-d', os.path.join(BASE_DIR, PACKAGE_DIR),
+                  '-r', os.path.join(BASE_DIR, REQUIREMENTS_DIR, 'requirements.txt')])
+        # Popen(f"pip download -d {os.path.join(BASE_DIR, PACKAGE_DIR)} -r {os.path.join(BASE_DIR, REQUIREMENTS_DIR, 'requirements.txt')}").wait()
+    files = os.listdir(os.path.join(BASE_DIR, PACKAGE_DIR))
+    if len(files) == 0:
+        return '', 500
+    return '', 204
+
+
+@app.route('/requirements/get_requirements', methods=['GET'])
+def get_requirements():
+    return send_file(get_zip_stream(), as_attachment=True,
+                     attachment_filename='requirements_dependencies.zip')
+
+
+@app.route('/post_dependencies', methods=['POST'])
+def post_dependencies():
+    args = pip_parser.parse_args()
+    package: str = args['package']
+    options: str = args['options']
+    create_clear_directories()
+    if options and package:
+        pip.main(['download',
+                  '-d', os.path.join(BASE_DIR, PACKAGE_DIR),
+                  options,
+                  package])
+        # Popen(f"pip download -d {os.path.join(BASE_DIR, PACKAGE_DIR)} {options} {package}").wait()
+    else:
+        pip.main(['download',
+                  '-d', os.path.join(BASE_DIR, PACKAGE_DIR),
+                  package])
+        # Popen(f"pip download -d {os.path.join(BASE_DIR, PACKAGE_DIR)} {package}").wait()
+    files = os.listdir(os.path.join(BASE_DIR, PACKAGE_DIR))
+    if len(files) == 0:
+        return '', 500
+    return '', 204
+
+
+@app.route('/get_dependencies', methods=['GET'])
 def get_dependencies():
     args = pip_parser.parse_args()
-    package = args['package']
-    options = args['options']
-    with tempfile.TemporaryDirectory() as temp:
-        if options:
-            pip.main(['download', '-d', os.path.join(temp, 'packages'), options, package])
-        else:
-            pip.main(['download', '-d', os.path.join(temp, 'packages'), package])
+    package: str = args['package']
 
-        files = os.listdir(os.path.join(temp, 'packages'))
-        if len(files) == 0:
-            return '', 500
-        memory_zip = io.BytesIO()
-        with zipfile.ZipFile(memory_zip, mode='w', compression=zipfile.ZIP_DEFLATED) as zip_file:
-
-            for file in files:
-                with open(os.path.join(temp, 'packages', file), mode='rb') as ff:
-                    zip_file.writestr(file, ff.read())
-        memory_zip.seek(0)
-
-        return send_file(memory_zip, as_attachment=True, attachment_filename=f'{package}.zip')   # return b64encode(bytes(memory_zip.getvalue())), 200
+    return send_file(get_zip_stream(), as_attachment=True,
+                     attachment_filename=f'{package}.zip')
 
 
 if __name__ == '__main__':
--- parsers.py
+++ parsers.py
@@ -1,8 +1,13 @@
 from flask_restplus import reqparse
+from werkzeug.datastructures import FileStorage
 
 pip_parser = reqparse.RequestParser()
 pip_parser.add_argument('package', type=str, required=True, help='Package name', location='args')
 pip_parser.add_argument('options', type=str, required=False, help='Options for pip', location='args')
+
+requirements_parser = reqparse.RequestParser()
+requirements_parser.add_argument('requirements', type=FileStorage, required=False, store_missing=False, help='Requirements file', location='files')
+requirements_parser.add_argument('options', type=str, required=False, help='Options for pip', location='form')
 
 download_parser = reqparse.RequestParser()
 download_parser.add_argument('package', type=str, required=True, help='Package name', location='args')
